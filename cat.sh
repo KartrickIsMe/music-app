7a17f3344cfd13c89b80528f2437ce3f4c7969f3
KartrickIsMe <kartikeys860@gmail.com>
2026-06-28 20:17:50 +0530

    Delete cat.sh

--- cat.sh
+++
@@ -1,7 +0,0 @@
-cat android/app/src/main/java/com/echoai/musicapp/MainActivity.java
-cat www/index.html
-cat www/app.js
-cat www/style.css
-cat android/app/build.gradle
-cat android/build.gradle
-cat android/app/src/main/AndroidManifest.xml
