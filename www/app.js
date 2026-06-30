5cbf3577b34966aa4b2109f0fc9792c61272ed1a
Kartikey <kartikeys860@gmail.com>
2026-06-30 17:20:01 +0530

    Use functions

--- www/app.js
+++ www/app.js
@@ -1,3 +1,12 @@
 window.onReply = function (message) {
   document.getElementById('output').textContent = message;
 };
+
+
+function buttonclick() {
+    document.getElementByid("button").addEventListener("click", invokeAndroid)
+}
+
+function invokeAndroid() {
+    window.Android.sayHello();
+}
